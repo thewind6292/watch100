a5a747b723e6ada26110d0712fd197e0e91518e1
chuoitaotau <vietanh.vnkingnet@gmail.com>
2017-02-01 10:30:54 +0700

    update backend

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'watch100');
+define('DB_NAME', '2017tt_watch100');
 
 /** MySQL database username */
 define('DB_USER', 'root');
