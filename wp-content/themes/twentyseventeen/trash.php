16df83f16e0585ed19c499a96f8ddbf7bde10939
anhnv <anhnv@qsoft.com.vn>
2017-02-03 17:59:36 +0700

    code list shop

---
+++ wp-content/themes/twentyseventeen/trash.php
@@ -0,0 +1,17 @@
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Thương Hiệu')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Khoảng Đường Kính')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Màu Sắc')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Kiểu Đồng Hồ')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Loại Dây')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Mức Giá')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Loại Đồng Hồ')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Giới Tính')) : else : ?><?php endif; ?>
+
+<?php if(function_exists('dynamic_sidebar') && dynamic_sidebar('Lọc SP Theo Khuyến Mại')) : else : ?><?php endif; ?>
