ee1d8a4f2d3102fa681821013491e482f74d86cf
Nguyen Viet Anh <anhnv@qsoft.com.vn>
2017-02-03 08:32:18 +0700

    commit footer

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -20,7 +20,6 @@
                     <div class="center-footer">
                         <div id="bottom-nav">
                             <ul>
-
                                 <li class=' none_ul level0 menu-item' >
                                     <?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
                                             <?php dynamic_sidebar( 'sidebar-2' ); ?>
@@ -44,19 +43,17 @@
                             </ul>
                         </div>    		
                     </div>
-
                     <div class="row">
                         <div class="col-sm-6 col-xs-12 col-lg-4 col-md-4">
                             <div class="title-col-ft">Thông tin liên hệ</div>
                             <div class="top-footer">
-                                <p>Quyền sở hữu &amp; Bản quyền Website n&agrave;y thuộc:<br />
-                                    <strong>C&Ocirc;NG TY PH&Acirc;N PHỐI SẢN PHẨM CAO CẤP LPD</strong><br />
-                                    Địa chỉ: 41 Phố Thi S&aacute;ch, Q. Hai B&agrave; Trưng, H&agrave; Nội<br />
-                                    Điện thoại: (+84-4) 3976 4617 | Fax: (+84-4) 3976 4618 &nbsp; &nbsp; &nbsp;<br />
-                                    Email: gallewatch@gmail.com<br />
-                                    Website: http://gallewatch.com</p>							<div class="registed">
-                                    <a href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=8590" target="_bank"><img src="http://www.gallewatch.com/images/dathongbao.png" /></a>
-                                </div>
+                                <p>
+                                Địa chỉ: <?php if(of_get_option('address_1')) {
+                                        echo of_get_option('address_1');} ?><br />
+                                Điện thoại: <?php if(of_get_option('phone_number_2')) {
+                                        echo of_get_option('phone_number_2');} ?><br />
+                                Email: <?php if(of_get_option('email')) {
+                                        echo of_get_option('email');} ?><br />
                             </div>
                         </div>
                         <div class="col-sm-6 col-xs-12 col-lg-4 col-md-4">
@@ -65,17 +62,6 @@
                                 <div class=" tf-item text-right">
                                     <div class='block_content discount_content'>
                                         <form id="discount_form" method="post" name="newletter_form" action="http://www.gallewatch.com/index.php?module=discount&amp;task=save" onsubmit="javascript: return check_discount_form();" >
-
-                                            <div class="wrapper-check cf">
-                                                <div class="item-check fl">
-                                                    <input type="checkbox" value="discount" checked class="check-footer" id="km" name="cat[]" />
-                                                    <label for="km">Tin khuyến mại</label>
-                                                </div>
-                                                <div class="item-check fl">
-                                                    <input type="checkbox" value="manufactory" class="check-footer" id="th" name="cat[]" />
-                                                    <label for="th">Tin thương hiệu</label>
-                                                </div>
-                                            </div>
                                             <div class="wrapper-discount">    
                                                 <input type="text" name="email" id="dc_email" placeholder="Nhập email..." class="txt-input"  />
                                                 <input type="submit" name="submit" value="Đăng ký" class="button-sub button" />
@@ -85,40 +71,37 @@
                                     </div>                            </div>
                                 <div class="social-ft">
                                     <div class="title-social">Kết nối với Gallewatch</div>
-                                    <a href="https://www.facebook.com/GalleWatch" target="_blink">
+                                    <a href="<?php if(of_get_option('link_fb')) {
+                                        echo of_get_option('link_fb');} ?>" target="_blink">
                                         <img   src="http://www.gallewatch.com/images/face.png"  />
                                     </a>
-                                    <a href="https://twitter.com/gallewatch" target="_blink">
+                                    <a href="<?php if(of_get_option('link_fb')) {
+                                        echo of_get_option('link_fb');} ?>" target="_blink">
                                         <img   src="http://www.gallewatch.com/images/tw.png"  />
                                     </a>
-                                    <a href="https://plus.google.com/u/0/+minhtuangallewatch" target="_blink"> 
+                                    <a href="<?php if(of_get_option('link_fb')) {
+                                        echo of_get_option('link_fb');} ?>" target="_blink"> 
                                         <img   src="http://www.gallewatch.com/images/gg.png"  />
                                     </a>
-                                    <a href="https://www.youtube.com/channel/UCr7hVndDMxNrbNkfpXEeuBQ" target="_blink"> 
+                                    <a href="<?php if(of_get_option('link_fb')) {
+                                        echo of_get_option('link_fb');} ?>" target="_blink"> 
                                         <img   src="http://www.gallewatch.com/images/youtube.png"  />
                                     </a>
-                                    <a href="mailto:gallewatch@gmail.com" target="_blink"> 
-                                        <img   src="http://www.gallewatch.com/images/mail.png"  />
-                                    </a>
-                                    <div class="wrapper-bt-social">
-                                        <span class='st_fblike_hcount' displayText='Facebook Like'></span>
-                                        <span class='st_plusone_hcount' displayText='Google +1'></span>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-6 col-xs-12 col-lg-4 col-md-4 group-fb">
-                            <div class="fb-page" data-adapt-container-width="false" data-height="210" data-hide-cover="false" data-href="https://www.facebook.com/GalleWatch/?fref=ts" data-show-facepile="true" data-small-header="false" data-tabs="timeline">
-                                <blockquote cite="https://www.facebook.com/GalleWatch/?fref=ts" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/GalleWatch/?fref=ts">Galle Watch</a></blockquote>
-                            </div>                    </div>
+                            <div class="fb-page" data-href="https://www.facebook.com/Watch100vn-%C4%90%E1%BB%93ng-h%E1%BB%93-ch%C3%ADnh-h%C3%A3ng-100-980592438728060/" data-tabs="timeline" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Watch100vn-%C4%90%E1%BB%93ng-h%E1%BB%93-ch%C3%ADnh-h%C3%A3ng-100-980592438728060/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Watch100vn-%C4%90%E1%BB%93ng-h%E1%BB%93-ch%C3%ADnh-h%C3%A3ng-100-980592438728060/">Watch100.vn - Đồng hồ chính hãng 100</a></blockquote></div>
+                        </div>
                     </div>
                     <div class="border-mainmenu border-ft"></div>
                     <div class="bottom-footer">
                         <div class=" bottom_info">
-                            <p style="text-align: justify;"><span style="color:#333333;"><span style="font-family: Arial, Helvetica, sans-serif;">Cửa h&agrave;ng Galle Watch chuy&ecirc;n</span></span><span style="color: rgb(139, 139, 139); font-family: Arial, Helvetica, sans-serif;">&nbsp;</span><span style="font-size:12px;"><span style="color:#999999;"><a href="http://www.gallewatch.com/dong-ho-nam-pc138.html" target="_blank">Đồng hồ nam</a><span style="font-family: Arial, Helvetica, sans-serif;">,&nbsp;</span><a href="http://www.gallewatch.com/dong-ho-nu-pc139.html" target="_blank">Đồng hồ nữ</a><span style="font-family: Arial, Helvetica, sans-serif;">,&nbsp;</span><a href="http://www.gallewatch.com/dong-ho-khac-pc140/loc-san-pham:dong-ho-doi.html" target="_blank">Đồng hồ đ&ocirc;i</a><span style="font-family: Arial, Helvetica, sans-serif;">,&nbsp;</span></span><a href="index.html" style="border: 0px; margin: 0px; padding: 0px; font-size: 12px; outline: none; text-decoration: none; color: rgb(255, 255, 255); font-family: Arial, Helvetica, sans-serif;" target="_blank" title="Đồng hồ đeo tay chính hãng hàng cao cấp"><span style="color:#999999;">Đồng hồ đeo tay ch&iacute;nh h&atilde;ng h&agrave;ng cao cấp</span></a><span style="color:#999999;"><span style="font-family: Arial, Helvetica, sans-serif;">,&nbsp;</span></span><a href="http://www.gallewatch.com/dong-ho-nam/f0-0-0-0-5-0-0-0.html" style="border: 0px; margin: 0px; padding: 0px; font-size: 12px; outline: none; text-decoration: none; color: rgb(255, 255, 255); font-family: Arial, Helvetica, sans-serif;" target="_blank" title="Đồng hồ Thụy Sỹ"><span style="color:#999999;">Đồng hồ Thụy Sỹ</span></a><span style="color:#999999;"><span style="font-family: Arial, Helvetica, sans-serif;">&nbsp;-&nbsp;</span></span></span><a href="http://www.gallewatch.com/gioi-thieu/galle-watch.html" style="border: 0px; margin: 0px; padding: 0px; font-size: 12px; outline: none; text-decoration: none; color: rgb(255, 255, 255); font-family: Arial, Helvetica, sans-serif;" target="_blank" title="Mua đồng hồ Thuỵ Sỹ chính hãng ở đâu"><span style="font-size:12px;"><span style="color:#999999;">Mua đồng hồ Thụy Sỹ ch&iacute;nh h&atilde;ng ở đ&acirc;u, GalleWatch online Google+</span>đồng hồ Thuỵ Sỹ ch&iacute;nh h&atilde;ng </span>ở đ&acirc;u</a><a href="https://plus.google.com/104910029901000977133?rel=author" style="border: 0px; margin: 0px; padding: 0px; font-size: 12px; outline: none; text-decoration: none; color: rgb(255, 255, 255); font-family: Arial, Helvetica, sans-serif;">Gallewatch online Google+</a></p>                    </div>
-                        <!--    			<div class="design-by pull-right">
-                                                                <a title="Thiết kế website" href="http://finalstyle.com">Thiết kế website: FinalStyle</a>
-                                                        </div>-->
+                            <p style="text-align: center;">
+                                <?php if(of_get_option('text_footer')) {
+                                        echo of_get_option('text_footer');} ?>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -138,33 +121,7 @@
         <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
         <script type="text/javascript">stLight.options({publisher: "c6652b23-5a6c-4da6-a3a0-d0cc1ca50cd2", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 
-        <script type='text/javascript'>window._sbzq || function (e) {
-                e._sbzq = [];
-                var t = e._sbzq;
-                t.push(["_setAccount", 13443]);
-                var n = e.location.protocol == "https:" ? "https:" : "http:";
-                var r = document.createElement("script");
-                r.type = "text/javascript";
-                r.async = true;
-                r.src = n + "//static.subiz.com/public/js/loader.js";
-                var i = document.getElementsByTagName("script")[0];
-                i.parentNode.insertBefore(r, i)
-            }(window);</script>
 
-        <script type="text/javascript">
-            /* <![CDATA[ */
-            var google_conversion_id = 964877199;
-            var google_custom_params = window.google_tag_params;
-            var google_remarketing_only = true;
-            /* ]]> */
-        </script>
-        <script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
-        </script>
-        <noscript>
-            <div style="display:inline;">
-                <img height="1" width="1" style="border-style:none;" alt="" src="http://googleads.g.doubleclick.net/pagead/viewthroughconversion/964877199/?guid=ON&amp;script=0"/>
-            </div>
-        </noscript>
         <script type="text/javascript" src="<?php echo get_theme_file_uri(); ?>/js/custom_js.js">
         <div style='width:1px;height:1px;display:none'><img src='http://ads.novanet.vn/adverclient.js?code=2237'/></div><div style="display:none;">1.06539 sec| 2160.391 kb</div><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/templates/default/js/bootstrap.min.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/templates/default/js/main.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/templates/default/js/form.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/templates/default/js/script.min.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/modules/home/assets/js/default.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/libraries/jquery/owl_carousel/owl-carousel/owl.carousel.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/blocks/slideshow/assets/js/progress_bar.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/blocks/partners/assets/js/progress_bar.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/blocks/products_list/assets/js/vertical.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/libraries/jquery/owl_carousel/owl-carousel/owl.carousel.min.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/modules/products/assets/js/jquery.ratingbar.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/modules/products/assets/js/jquery.raty.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/libraries/jquery/jquery.ui/jquery-ui.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/blocks/search/assets/js/search.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/blocks/mainmenu/assets/js/default.js"></script><script language="javascript" type="text/javascript" src="http://www.gallewatch.com/blocks/mainmenu/assets/js/bottommenu.js"></script>
 <?php wp_footer(); ?>
--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -32,6 +32,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.8";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 	<?php global $woocommerce; ?>
 <div class="full"></div>
         <div class="content-buy-fast cf" id="wrapper-buy-fast"></div>
