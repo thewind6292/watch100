072b8c9d62a19d797069463acdadd98a63e4a0a5
Nguyen Viet Anh <anhnv@qsoft.com.vn>
2017-02-05 15:50:24 +0700

    15:50 5-2-2017: contact form

--- wp-content/themes/twentyseventeen/functions.php
+++ wp-content/themes/twentyseventeen/functions.php
@@ -18,6 +18,30 @@ if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
 	return;
 }
+//include_once 'include/Facebook/autoload.php';
+add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
+
+function custom_override_checkout_fields($fields) {
+    $fields['billing']['billing_first_name']['label'] = 'Họ tên';
+    $fields['billing']['billing_email']['label'] = 'Email';
+    $fields['billing']['billing_address_1']['label'] = 'Địa chỉ';
+    $fields['billing']['billing_phone']['label'] = 'Số điện thoại';
+    
+//    unset($fields['billing']['billing_first_name']);
+    unset($fields['billing']['billing_last_name']);
+    unset($fields['billing']['billing_company']);
+//    unset($fields['billing']['billing_address_1']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_state']);
+//    unset($fields['billing']['billing_phone']);
+//    unset($fields['order']['order_comments']);
+//    unset($fields['billing']['billing_email']);
+  
+    return $fields;
+}
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
