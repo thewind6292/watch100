8ec9f030b928921eacecc5159ad5c38d935a7bd1
anhnv <anhnv@qsoft.com.vn>
2017-02-06 08:40:25 +0700

    push new day

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '123456');
 // define('DB_PASSWORD', '');
 /** MySQL hostname */
-define('DB_HOST', '192.168.1.24');
+define('DB_HOST', 'localhost');
 // define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
